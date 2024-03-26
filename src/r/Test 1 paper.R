2a24b3a2250e0f09daecacf7c94e29fe38960e4f
soliprem <franci.solidoro@gmail.com>
2024-03-26 17:16:05 +0100

    moving shit around

--- src/r/Test 1 paper.R
+++ src/r/Test 1 paper.R
@@ -1,5 +1,5 @@
 # gun death dataset WIP (summing values by year and state)
-gun_deaths_us_1999_2019 <- read.csv("gun_deaths_us_1999_2019.csv")
+gun_deaths_us_1999_2019 <- read.csv("../../data/gun_deaths_us_1999_2019.csv")
 gun_deaths_us_1999_2019 <- as.data.frame(gun_deaths_us_1999_2019)
 gundeaths_cut <- gun_deaths_us_1999_2019[-c(1, 3:5, 7)]
 gundeaths_cut <- subset(gundeaths_cut, !gundeaths_cut$Year > 2017)
@@ -28,15 +28,17 @@ proc_lawprov <- preProcess(as.data.frame(law_provision_norm$lawtotal),
 law_provision_norm$index <- predict(proc_lawprov, as.data.frame(law_provision_norm$lawtotal))
 View(law_provision_norm)
 # merging
-merged_guns <- data.frame()
+merged_guns <- data.frame(50, 50)
 deaths <- 0
-merged_guns$deaths <- 0
-merged_guns$population <- 0
+merged_guns[, deaths] <- 0
+merged_guns[, population] <- 0
 for (i in nrow(gundeaths_cut)) {
     state <- gundeaths_cut$State_Name[i]
     year <- gundeaths_cut$Year[i]
-    if (gundeaths_cut$State_Name[i] %in% merged_guns$state) {
-        merged_guns$deaths[gundeaths_cut$State_Name[i]] <- merged_guns$deaths[gundeaths_cut$State_Name[i]] + gundeaths_cut$Deaths[i]
-        merged_guns$population[gundeaths_cut$State_Name[i]] <- merged_guns$population[gundeaths_cut$State_Name[i]] + gundeaths_cut$Population
+    county_deaths <- gundeaths_cut[i, "Deaths"]
+    county_population <- gundeaths_cut[i, "Population"]
+    if (state %in% merged_guns$state) {
+        merged_guns$deaths[state] <- merged_guns$deaths[state] + gundeaths_cut$Deaths[i]
+        merged_guns$population[state] <- merged_guns$population[state] + gundeaths_cut$Population[i]
     }
 }
