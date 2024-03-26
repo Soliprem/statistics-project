29a32afbba2e7f227316701f044fdb449d3ea7be
soliprem <franci.solidoro@gmail.com>
2024-03-26 16:45:01 +0100

    beginning of merge

--- Test 1 paper.R
+++ Test 1 paper.R
@@ -1,5 +1,6 @@
 # gun death dataset WIP (summing values by year and state)
-gun_deaths_us_1999_2019 <- as.data.frame("gun_deaths_us_1999_2019")
+gun_deaths_us_1999_2019 <- read.csv("gun_deaths_us_1999_2019.csv")
+gun_deaths_us_1999_2019 <- as.data.frame(gun_deaths_us_1999_2019)
 gundeaths_cut <- gun_deaths_us_1999_2019[-c(1, 3:5, 7)]
 gundeaths_cut <- subset(gundeaths_cut, !gundeaths_cut$Year > 2017)
 gundeaths_cut <- gundeaths_cut[-c(5:10)]
@@ -11,7 +12,7 @@ v_year <- gundeaths_cut$Year == 1999
 v_filter <- v_state & v_year
 v_check <- (v_filter)
 spec_sum <- function(selected_year, selected_state) {
-  colSums(gundeaths_cut[, 3:4])
+    colSums(gundeaths_cut[, 3:4])
 }
 View(gundeaths_cut)
 
@@ -22,9 +23,20 @@ law_provision_norm <- as.data.frame(law_provision_norm)
 law_provision_norm <- subset(law_provision_norm, !law_provision_norm$year < 1999)
 library(caret)
 proc_lawprov <- preProcess(as.data.frame(law_provision_norm$lawtotal),
-  method = c("range")
+    method = c("range")
 )
 law_provision_norm$index <- predict(proc_lawprov, as.data.frame(law_provision_norm$lawtotal))
 View(law_provision_norm)
-
 # merging
+merged_guns <- data.frame()
+deaths <- 0
+merged_guns$deaths <- 0
+merged_guns$population <- 0
+for (i in nrow(gundeaths_cut)) {
+    state <- gundeaths_cut$State_Name[i]
+    year <- gundeaths_cut$Year[i]
+    if (gundeaths_cut$State_Name[i] %in% merged_guns$state) {
+        merged_guns$deaths[gundeaths_cut$State_Name[i]] <- merged_guns$deaths[gundeaths_cut$State_Name[i]] + gundeaths_cut$Deaths[i]
+        merged_guns$population[gundeaths_cut$State_Name[i]] <- merged_guns$population[gundeaths_cut$State_Name[i]] + gundeaths_cut$Population
+    }
+}
