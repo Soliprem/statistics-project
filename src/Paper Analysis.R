ae6ece51fe79fd4d2c8fbc8f3b846af755f6db60
soliprem <franci.solidoro@gmail.com>
2024-03-27 16:50:52 +0100

    refactoring

--- src/Paper Analysis.R
+++ src/Paper Analysis.R
@@ -1,9 +1,9 @@
-#libraries used
+# libraries used
 library(caret)
 library(dplyr)
 
-# gun death dataset 
-gun_deaths_us_1999_2019 <- read.csv("gun_deaths_us_1999_2019.csv")
+# gun death dataset
+gun_deaths_us_1999_2019 <- read.csv("../data/gun_deaths_us_1999_2019.csv")
 gun_deaths_us_1999_2019 <- as.data.frame(gun_deaths_us_1999_2019)
 gundeaths_cut <- gun_deaths_us_1999_2019[-c(1, 3:5, 7)]
 gundeaths_cut <- subset(gundeaths_cut, !gundeaths_cut$Year > 2017)
@@ -11,18 +11,18 @@ gundeaths_cut <- gundeaths_cut[-c(5:10)]
 View(gundeaths_cut)
 
 gundeaths_cut$Year <- as.character(gundeaths_cut$Year)
-gundeaths_cond <- gundeaths_cut %>% 
-  group_by(across(where(is.character))) %>%
-  summarise(across(where(is.numeric), sum, na.rm = T ), .groups = "drop")
+gundeaths_cond <- gundeaths_cut %>%
+    group_by(across(where(is.character))) %>%
+    summarise(across(where(is.numeric), sum, na.rm = T), .groups = "drop")
 
 gundeaths_cond$Rate <- 0
- for(i in 1:nrow(gundeaths_cond)){
-  r <- ((gundeaths_cond$Deaths[i] / gundeaths_cond$Population[i])*100000)
-  gundeaths_cond$Rate[i] <- r
+for (i in 1:nrow(gundeaths_cond)) {
+    r <- ((gundeaths_cond$Deaths[i] / gundeaths_cond$Population[i]) * 100000)
+    gundeaths_cond$Rate[i] <- r
 }
 View(gundeaths_cond)
 
-# law provision dataset 
+# law provision dataset
 law_provision_norm <- read.csv("law_provision_norm.csv")
 law_provision_norm <- as.data.frame(law_provision_norm)
 law_provision_norm <- subset(law_provision_norm, !law_provision_norm$year < 1999)
@@ -34,8 +34,10 @@ View(law_provision_norm)
 
 # merging
 gundeaths_cond <- gundeaths_cond %>%
-  rename("state" = "State_Name", 
-         "year" = "Year")
+    rename(
+        "state" = "State_Name",
+        "year" = "Year"
+    )
 
-merged <- merge(law_provision_norm, gundeaths_cond, by = c("state","year"))
+merged <- merge(law_provision_norm, gundeaths_cond, by = c("state", "year"))
 View(merged)
